be6ecb8c15c8eaa37465b3b0fc15913e73f1cdd8
devjosegomez <devjosegomez@gmail.com>
2020-06-09 06:19:46 -0500

    Challenge location

--- HelloWorld.js
+++ HelloWorld.js
@@ -3,3 +3,9 @@ let firstName = "José"
 let lastName = "Gómez"
 let fullName = firstName + ' ' + lastName
 console.log ( "by " + fullName )
+
+// "Challenge" city, country, location
+let city = "Guadalajara"
+let country = "Mexico"
+let location = city + ", " + country
+console.log ( location )
