417f2b3a34334b028afcfa5bc811a1834c899789
devjosegomez <devjosegomez@gmail.com>
2020-06-10 06:11:24 -0500

    Challenge 'discount'

--- basics/conditionals.js
+++ basics/conditionals.js
@@ -1,14 +1,13 @@
-// Challenge
 let age = 28
 let message = ''
 
 if ( age <= 7 ) 
 {
-    message = "You are a child"
+    message = "You are a child, you have 30% OFF discount!"
 }
 else 
 {
-    message = "You are a senior"
+    message = "You are a senior, you have 50% OFF discount!"
 }
 
 console.log ( message )
