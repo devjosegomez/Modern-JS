ebd7edd0e4fb2907e575bde5262f0e7b18062da7
devjosegomez <devjosegomez@gmail.com>
2020-06-16 11:38:51 -0500

    Arrow function = () =>

--- functions/functions-101.js
+++ functions/functions-101.js
@@ -9,3 +9,8 @@ let square = (number) =>
     return number * number
 }
 console.log ( square ( 3 ) )
+
+//Challange Convert F to C
+let fahrenheitToCelcius = ( tempeture ) => ( tempeture - 32 ) * 5/9
+
+console.log ( fahrenheitToCelcius ( 50 ) ) //Should return 10
